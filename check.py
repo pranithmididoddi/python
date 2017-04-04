2479884b86652775e0de8ce2a60571128fca1057
Srinivas <pranith13@gmail.com>
2017-04-04 17:03:39 -0500

    numpy

--- check.py
+++ check.py
@@ -7,3 +7,13 @@ import numpy as np
 b=np.array([[1,2,3],[4,5,6]])
 print b.shape
 print b[1,2]
+
+#List
+
+mylist=[3,1,2]
+print type(mylist)
+
+myset=set([3,2,1])
+myset.add(3)
+print type(myset)
+print myset

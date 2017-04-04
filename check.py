9f5a0e67ab5768ff27ae397a2f264ac5ba2fb2de
Srinivas <pranith13@gmail.com>
2017-04-04 15:26:26 -0500

    numpy

--- check.py
+++ check.py
@@ -1,5 +1,9 @@
 import numpy as np
 
-a=np.array([1,2,3])
-print type(a)
-print a.shape
+# a=np.array([1,2,3])
+# print type(a)
+# print a.shape
+
+b=np.array([[1,2,3],[4,5,6]])
+print b.shape
+print b[1,2]

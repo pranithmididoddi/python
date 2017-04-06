62bef3af43b49407d3a85cb4f5f261afbc95d148
Srinivas <pranith13@gmail.com>
2017-04-06 11:39:39 -0500

    calling functions in python

--- callingfn.py
+++ callingfn.py
@@ -1,3 +1,4 @@
+#printing the name
 def printname(name, number):
     print("The name is: ",name)
     print("The number is: ",number)
@@ -9,3 +10,20 @@ def printname(name, number):
 printname("pranith", 57)
 
 #Checking for the github
+
+#checking for the prime number
+
+def findprime(num):
+    if num>1:
+        for i in range(2,num-1):
+            if num%i==0:
+                print("The number is not prime")
+                break;
+            else:
+                print("The number is prime")
+                break;
+    else:
+        print("The number is prime")
+
+
+findprime(13)

b0aec84cb5b9ec93fc76bff23023b139cc134a0d
Srinivas <pranith13@gmail.com>
2017-04-06 00:07:07 -0500

    calling functions in python

---
+++ callingfn.py
@@ -0,0 +1,9 @@
+def printname(name, number):
+    print("The name is: ",name)
+    print("The number is: ",number)
+
+    if name=="pranith":
+        print("Hello, ",name)
+
+
+printname("pranith", 57)
